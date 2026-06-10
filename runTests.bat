aa3219da7b25dfcb91d714682ff64d08f34ecdda
Balaji-0801 <balaji.sundaram.bde@gmail.com>
2026-06-10 14:36:07 +0530

    Add files via upload

---
+++ runTests.bat
@@ -0,0 +1,4 @@
+echo [Basic] >> c:/programdata/metamation/flux/recent/notes.ini
+echo LicenseServer=192.168.1.183 >> c:/programdata/metamation/flux/recent/notes.ini
+md c:/mm/bin/
+pause >> c:/mm/Fluxtester/bin/Fluxtester.exe -t sanity -noreport 
